70f5a264fd26d9eddcde01004a30de06a1f34d2d
Neeraj Dalal <nd941z@gmail.com>
2021-05-29 08:13:00 +0530

    210529-024300

--- bin/main.sh
+++ bin/main.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
 
-########################################
-
-owner="nrjdalal"
-repository="xamyr"
-callname="xmyr"
-
-host="https://raw.githubusercontent.com"
-path="/$owner/$repository/master"
-url="$host$path"
-
-bin="$url/bin"
-lib="$url/lib"
-
-utilities="$(mktemp)"
-curl -fsSL $url/utils.sh >$utilities
-source $utilities
-rm $utilities
-
-########################################
+echo "xmyrrrrrrrr"
