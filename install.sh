112baa51ad6c963dcc7dad072b9fa85514f5e231
Neeraj Dalal <nd941z@gmail.com>
2021-05-29 08:12:34 +0530

    210529-024234

--- install.sh
+++ install.sh
@@ -12,4 +12,19 @@ resources="$(mktemp -u).$alias"
 curl -fsSL $url/resources.sh >$resources
 source $resources
 
-echo $red/bleed/$green/lol$off
+if [[ -z "${BASH_VERSION}" ]]; then
+  echo "Bash is required to interpret this script!"
+  exit 1
+fi
+
+if [[ "$(uname)" != "Linux" ]]; then
+  echo "$repository is only supported on Linux as of now."
+  exit 1
+fi
+
+_ONLYSUDO_
+
+curl -fsSL $url/bin/main.sh >/usr/bin/$alias
+chmod +x /usr/bin/$alias
+
+echo "${green}$repository is successfully installed.${off}"
